360d1d949712b14c93e69a63652f41b051ccbd9f
George Spearing <george.spearing@uvm.edu>
2021-11-28 08:35:51 -0500

    initialize app

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by user203264 on 11/28/21.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
