89cb7cf823bda29c1550199ccfe885a9bad26f39
George Spearing <george.spearing@uvm.edu>
2021-11-28 13:32:47 -0500

    Adding image store and detail image view

---
+++ Photorama/Photorama/ImageStore.swift
@@ -0,0 +1,59 @@
+//
+//  ImageStore.swift
+//  AnimalSnap
+//
+//  Created by user203264 on 11/4/21.
+//
+
+import UIKit
+
+class ImageStore {
+    let cache = NSCache<NSString,UIImage>()
+    
+    func setImage(_ image: UIImage, forKey key: String){
+        cache.setObject(image, forKey: key as NSString)
+        
+        // create full url for image
+        let url = imageURL(forKey: key)
+        
+        // turn image into jpeg data
+        if let data = image.jpegData(compressionQuality: 0.5){
+            // write it to full url
+            try? data.write(to: url)
+        }
+    }
+    
+    func image(forKey key: String)-> UIImage? {
+//        return cache.object(forKey: key as NSString)
+        if let existingImage = cache.object(forKey: key as NSString){
+            return existingImage
+        }
+        let url = imageURL(forKey: key)
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else{
+            return nil
+        }
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        return imageFromDisk
+    }
+    
+    func deleteImage(forKey key: String){
+        cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(forKey: key)
+        
+        do{
+            try FileManager.default.removeItem(at: url)
+        } catch{
+            print("Error removing the image from disk: \(error)")
+        }
+    }
+    
+    func imageURL(forKey key: String) -> URL {
+        let documentsDirectories =
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        
+        return documentDirectory.appendingPathComponent(key)
+    }
+    
+}
