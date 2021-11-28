04ffa57dac48f43b2304332bc3d695b6e7aac29d
George Spearing <george.spearing@uvm.edu>
2021-11-28 12:51:07 -0500

    adding collection view and activity indicator

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by user203264 on 11/28/21.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(displaying image: UIImage?){
+        if let imageToDisplay = image{
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else{
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
---
+++ Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by user203264 on 11/28/21.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource{
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let identifier = "PhotoCollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PhotoCollectionViewCell
+        cell.update(displaying: nil)
+        
+        return cell
+    }
+}
